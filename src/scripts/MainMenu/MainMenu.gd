a4285156fed750564589504511cae26d9b8c09c0
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2025-08-20 22:42:36 +0800

    feat: add temporary splash animation

--- src/scripts/MainMenu/MainMenu.gd
+++ src/scripts/MainMenu/MainMenu.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@export var quit_button: Button
+
+func _ready() -> void:
+	if quit_button:
+		quit_button.pressed.connect(_on_quit_button_pressed)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
--- src/scripts/Splash/SplashScreen.gd
+++ src/scripts/Splash/SplashScreen.gd
@@ -1,8 +1,10 @@
 extends Node
 
-func _ready():
-	go_main_screen()
+
+func _on_animation_player_animation_finished(anim_name):
+	go_main_screen();
+
 
 func go_main_screen():
-	await get_tree().create_timer(2.0).timeout
 	get_tree().change_scene_to_file("res://src/scenes/MainMenu.tscn")
+
