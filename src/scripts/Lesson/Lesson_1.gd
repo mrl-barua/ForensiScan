2b03a1d9a690acb25b0bf225ae8c74a51709534a
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2025-08-21 12:48:46 +0800

    feat: add lesson navigation and next button functionality

--- src/scripts/Lesson/Lesson_1.gd
+++ src/scripts/Lesson/Lesson_1.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var lesson_1_text: String = "Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id. Placerat in id cursus mi pretium tellus duis. Pretium tellus duis convallis tempus leo eu aenean."
 @onready var label: RichTextLabel = $Label
+@onready var next_button: Button = $NextButton
 var typewriter: Typewriter
 
 func _ready():
@@ -13,4 +14,7 @@ func _ready():
 	
 func _on_typing_done():
 	print("Typing finished!")
-
+	next_button.show();
+	
+func _on_next_button_pressed():
+	get_tree().change_scene_to_file("res://src/scenes/Lesson/Lesson_1.1.tscn")
