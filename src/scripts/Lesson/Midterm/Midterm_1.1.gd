53e5f7c58c07dcdea556018e2ed3c72d3fb7a3b4
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2025-09-01 23:35:28 +0800

    feat: add midterm lesson scenes and scripts
    - Created new scene files for Midterm lessons 1.1 to 1.55, including necessary nodes and resources.
    - Implemented basic script structure for each lesson, ensuring they extend Node2D and include _ready and _process functions.
    - Added a typewriter effect for the header text in Midterm 1.1 to enhance user experience.
    - Set up navigation controls for transitioning between lessons.

---
+++ src/scripts/Lesson/Midterm/Midterm_1.1.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var header_text: String = "HOW TO TAKE FINGERPRINTS"
+@onready var header: RichTextLabel = $VBoxContainer/Header
+@onready var navigation_buttons: Control = $NavigationControls
+var typewriter: Typewriter
+
+func _ready():
+	typewriter = Typewriter.new()
+	add_child(typewriter)  
+	
+	typewriter.connect("typing_finished", Callable(self, "_on_typing_done"))
+	typewriter.start_typing(header, header_text)
+	
+func _on_typing_done():
+	print("Typing finished!")
+	navigation_buttons.show();
+	
