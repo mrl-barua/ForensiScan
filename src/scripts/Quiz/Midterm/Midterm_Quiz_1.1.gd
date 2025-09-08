b89b95711eee6f61bd77a72fe3e48347dd9ff1e8
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2025-09-08 21:06:50 +0800

    feat: update Midterm Quiz 1.1 with structured questions and improved variable naming

--- src/scripts/Quiz/Midterm/Midterm_Quiz_1.1.gd
+++ src/scripts/Quiz/Midterm/Midterm_Quiz_1.1.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+@onready var question_1: Button = $Question_1
+@onready var question_2: Button = $Question_2
+@onready var question_3: Button = $Question_3
+@onready var question_4: Button = $Question_4
+@onready var question_5: Button = $Question_5
+@onready var question_6: Button = $Question_6
+@onready var question_7: Button = $Question_7
+@onready var question_8: Button = $Question_8
+@onready var question_9: Button = $Question_9
+@onready var question_10: Button = $Question_10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
